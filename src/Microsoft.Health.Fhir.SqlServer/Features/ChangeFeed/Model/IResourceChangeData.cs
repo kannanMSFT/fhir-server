d7517a4fca81919567e77b3174d2e7a91edc603d
Kannan Sundararajan <v-kannans@microsoft.com>
2021-05-22 23:31:46 -0700

    Update Microsoft.Health.Abstractions and Microsoft.Health.SqlServer pkgs. Add Feature Change feed and move the Data Adapter to read resource changes record from FHIR db.

---
+++ src/Microsoft.Health.Fhir.SqlServer/Features/ChangeFeed/Model/IResourceChangeData.cs
@@ -0,0 +1,24 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace Microsoft.Health.Fhir.SqlServer.Features.ChangeFeed.Model
+{
+    public interface IResourceChangeData
+    {
+        long Id { get; set; }
+
+        DateTime Timestamp { get; set; }
+
+        string ResourceId { get; set; }
+
+        short ResourceTypeId { get; set; }
+
+        int ResourceVersion { get; set; }
+
+        short ResourceChangeTypeId { get; set; }
+    }
+}
---
+++ src/Microsoft.Health.Fhir.SqlServer/Features/ChangeFeed/SqlResourceChangeDataReadOnlyDataStore.cs
@@ -0,0 +1,82 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading;
+using System.Threading.Tasks;
+using EnsureThat;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Logging;
+using Microsoft.Health.Abstractions.Data;
+using Microsoft.Health.Fhir.SqlServer.Features.ChangeFeed.Model;
+using Microsoft.Health.SqlServer;
+
+namespace Microsoft.Health.Fhir.SqlServer.Features.ChangeFeed
+{
+    public class SqlResourceChangeDataReadOnlyDataStore : ISource<IResourceChangeData>
+    {
+        private readonly ISqlConnectionFactory _sqlConnectionFactory;
+        private readonly ILogger<SqlResourceChangeDataReadOnlyDataStore> _logger;
+
+        public SqlResourceChangeDataReadOnlyDataStore(ISqlConnectionFactory sqlConnectionFactory, ILogger<SqlResourceChangeDataReadOnlyDataStore> logger)
+        {
+            EnsureArg.IsNotNull(sqlConnectionFactory, nameof(sqlConnectionFactory));
+            EnsureArg.IsNotNull(logger, nameof(logger));
+
+            _sqlConnectionFactory = sqlConnectionFactory;
+            _logger = logger;
+        }
+
+        public async Task<IReadOnlyCollection<IResourceChangeData>> FetchRecordsAsync(long startId, int pageSize, int? delayMilliseconds, CancellationToken cancellationToken)
+        {
+            EnsureArg.IsGte(startId, 0, nameof(startId));
+            EnsureArg.IsGte(pageSize, 0, nameof(pageSize));
+
+            var listResourceChangeData = new List<ResourceChangeData>();
+            try
+            {
+                using (SqlConnection sqlConnection = await _sqlConnectionFactory.GetSqlConnectionAsync(cancellationToken: cancellationToken))
+                {
+                    await sqlConnection.OpenAsync(cancellationToken);
+                    using (SqlCommand sqlCommand = new SqlCommand("dbo.FetchResourceChangeData", sqlConnection))
+                    {
+                        sqlCommand.CommandType = CommandType.StoredProcedure;
+                        sqlCommand.Parameters.AddWithValue("@startId", SqlDbType.BigInt).Value = startId;
+                        sqlCommand.Parameters.AddWithValue("@pageSize", SqlDbType.Int).Value = pageSize;
+                        if (delayMilliseconds.HasValue)
+                        {
+                            sqlCommand.Parameters.AddWithValue("@delayMilliseconds", SqlDbType.Int).Value = delayMilliseconds;
+                        }
+
+                        using (SqlDataReader sqlDataReader = await sqlCommand.ExecuteReaderAsync(CommandBehavior.SequentialAccess, cancellationToken))
+                        {
+                            while (await sqlDataReader.ReadAsync(cancellationToken))
+                            {
+                                listResourceChangeData.Add(new ResourceChangeData
+                                {
+                                    Id = (long)sqlDataReader["Id"],
+                                    Timestamp = (DateTime)sqlDataReader["Timestamp"],
+                                    ResourceId = (string)sqlDataReader["ResourceId"],
+                                    ResourceTypeId = (short)sqlDataReader["ResourceTypeId"],
+                                    ResourceVersion = (int)sqlDataReader["ResourceVersion"],
+                                    ResourceChangeTypeId = (short)sqlDataReader["ResourceChangeTypeId"],
+                                });
+                            }
+                        }
+
+                        return listResourceChangeData;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error from SQL database on FetchResourceChangeData");
+                throw;
+            }
+        }
+    }
+}
